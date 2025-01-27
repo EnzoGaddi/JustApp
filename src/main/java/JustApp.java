626a15709ec9004c5a9f2120702127d4dad66223
ENZOGADDI-STUDENT <G00431922@atu.ie>
2025-01-27 11:21:36 +0000

    First test run

---
+++ src/main/java/JustApp.java
@@ -0,0 +1,15 @@
+public class JustApp {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Just App");
+        System.out.println("This is your order....");
+        //Create an order
+
+
+
+        //List all of the food items like burgers, pizzas, fries etc
+
+        //Display details to the user
+    }
+}
